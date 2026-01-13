e2b11cc6b13ed63f51360fa0ba4a2612c5d15576
swapnachari-marjuk <marjukmujaddedi@gmail.com>
2026-01-13 23:10:58 +0600

    delted a page.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,17 +1,14 @@
 import Categories from "@/Components/Home/Categories/Categories";
 import Cta from "@/Components/Home/CTA/Cta";
-import FeaturedChallenges from "@/Components/Home/FeaturedChallenges/FeaturedChallenges";
 import Hero from "@/Components/Home/Hero/Hero";
 import HowItWorks from "@/Components/Home/HowItWorks/HowItWorks";
 import WhatIsSkillSprint from "@/Components/Home/WhatIsSkillSprint/WhatIsSkillSprint";
 import WhySkillSprint from "@/Components/Home/WhySkillSprint/WhySkillSprint";
-import Image from "next/image";
 
 export default function Home() {
   return <div>
     <Hero />
     <WhatIsSkillSprint />
-    <FeaturedChallenges />
     <Categories />
     <HowItWorks />
     <WhySkillSprint />
